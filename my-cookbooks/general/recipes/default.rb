2011ae591a743a308d799c744eaf4f751ce15402
Baldur Gudbjornsson <baldur.gudbjornsson@gmail.com>
2014-07-16 11:51:44 -0400

    Stuff

--- my-cookbooks/general/recipes/default.rb
+++ my-cookbooks/general/recipes/default.rb
@@ -7,7 +7,21 @@
 # All rights reserved - Do Not Redistribute
 #
 
+file "/etc/profile.d/path.sh" do
+  content "export PATH=/home/baldur/apps/genymotion:$PATH"
+  mode 00755
+end
+
 file "/etc/profile.d/git.sh" do
   content "export GIT_EDITOR=vim"
   mode 00755
 end
+
+file "/etc/profile.d/aliases.sh" do
+  content <<-DOC
+alias gcb="git checkout -b"
+DOC
+
+  mode 00755
+end
+
